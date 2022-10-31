9ef008d6479868d35234dd7691f20884c6007258
BC Ko <bcko@users.noreply.github.com>
2022-10-30 20:04:36 -0700

    update

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    newNextLinkBehavior: true,
+    scrollRestoration: true,
+  },
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+  assetPrefix: './'
+}
+
+module.exports = nextConfig
